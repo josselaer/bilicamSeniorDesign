f8d393cd390bc53cd2482a88aa1e878ebf3b9390
josselaer <josselaer@smu.edu>
2017-03-27 18:49:54 -0500

    creates csv file needs download test

--- Backend/form.py
+++ Backend/form.py
@@ -7,6 +7,8 @@ import json
 import urllib.parse
 import tornado.httpserver
 import ssl
+import csv
+from random import randint
 
 db = motor.motor_tornado.MotorClient().Bilirubin
 
@@ -51,18 +53,24 @@ class SearchByBiliHandler(tornado.web.RequestHandler):
         num2 = float(data["num2"][0])
         cursor = db.patients.find({"bilirubin":{"$gt":num1, "$lt":num2}})
         document = await cursor.to_list(length=100)
+        filename = bili_to_csv(document)
+        return filename
 
 class SearchByNameHandler(tornado.web.RequestHandler):
     async def get(self):
         data = urllib.parse.parse_qs(self.request.query)
         name = str(data["name"][0]).strip()
         document = await db.patients.find_one({"name":name})
+        filename = bili_to_csv(document)
+        return filename
 
 class SearchByIdHandler(tornado.web.RequestHandler):
     async def get(self):
         data = urllib.parse.parse_qs(self.request.query)
         num = int(data["idNum"][0])
         document = await db.patients.find_one({"id":num})
+        filename = bili_to_csv(document)
+        return filename
 
 class SearchByEthnicityHandler(tornado.web.RequestHandler):
     async def get(self):
@@ -70,11 +78,40 @@ class SearchByEthnicityHandler(tornado.web.RequestHandler):
         ethnicities = [x.title() for x in data["ethnicities[]"]]
         cursor = db.patients.find({"ethnicity":{"$in":ethnicities}})
         document = await cursor.to_list(length=100)
+        filename = bili_to_csv(document)
+        return filename
 
 class LogoutHandler(tornado.web.RequestHandler):
     def get(self):
         self.clear_cookie("User")
         self.redirect("/")
+
+def bili_to_csv(json_obj):
+    filename = "csv_download/" + str(randint(100,999999)) + ".csv"
+    csv_txt = "Name,ID,Bilirubin Value,Ethnicity,Date,Images\n"
+    if(type(json_obj) == list):
+        for x in json_obj:
+            csv_txt = csv_txt + json_to_csv(x)
+    elif(type(json_obj) == dict):
+        csv_txt = csv_txt + json_to_csv(json_obj)
+    else:
+        csv_txt = "error"
+
+    f = open(filename, 'w')
+    f.write(csv_txt)
+    f.close()
+    return filename
+
+def json_to_csv(json_obj):
+    csv_txt = ""
+    name = json_obj['name']
+    p_id = json_obj['id']
+    bilirubin = json_obj['bilirubin']
+    ethnicity = json_obj['ethnicity']
+    date = "1/1/95" #json_obj['date']
+    image = json_obj['images']
+    csv_txt = name + "," + str(p_id) + "," + str(bilirubin) + "," + ethnicity + "," + date + "," + image + "\n"
+    return csv_txt
 
 settings = {
     "template_path":os.path.dirname(os.path.realpath(__file__)) + "\\website\\",
